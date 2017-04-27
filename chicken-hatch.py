477b01cdfe04969584c30eab4dd8be9c4ada47df
Joe Well <1boy1dr@gmail.com>
2017-04-27 11:00:46 -0400

    change some arguments and add some new ones

--- chicken-hatch.py
+++ chicken-hatch.py
@@ -22,13 +22,16 @@ hatch.add_argument("-n", "--name", required=True,
                    help="name of package to install")
 hatch.add_argument("-v", "--package-version", required=True,
                    help="version of package to be installed")
-source_location = hatch.add_mutually_exclusive_group(required=True)
+source_location = hatch.add_mutually_exclusive_group()
 source_location.add_argument("-t", "--tar", metavar="ARCHIVE",
                              help="use tar to extract source from an archive")
-source_location.add_argument("-x", "--extract-only", metavar="ARCHIVE",
-                             help=("use tar to extract source from an archive "
-                                  "and don't compile the package"))
 source_location.add_argument("-d", "--source-directory", metavar="DIRECTORY",
                              help=("build package from it's source is the "
                              "given directory"))
+hatch.add_argument("-b", "--build", metavar="", help="build the package")
+hatch.add_argument("-i", "--install", metavar="", help="install the package")
+hatch.add_argument("-e", "--eggfile",
+                   help=("the instructions for building and installing the "
+                         "package.  if no eggfile is given the defualt one "
+                         "will be used"))
 args = parser.parse_args()
