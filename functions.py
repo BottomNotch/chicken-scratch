c0cf6435bd546b88259c723e030143b679be153b
Joe Well <1boy1dr@gmail.com>
2017-05-04 14:57:49 -0400

    add a build function and remove a line in the extract function

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
 import subprocess
+from pathlib import Path
+import os
 
 def extract(tarbell):
-#    returncode = 0
     try:
         subprocess.run(["tar", "-xvf", tarbell], check=True)
     except subprocess.CalledProcessError as e:
@@ -9,3 +10,40 @@ def extract(tarbell):
         print("error: tar exited with code " + str(returncode))
 
     return returncode
+
+def build(source_root : Path, seperate_build_dir : bool, config_command : list,
+          config_only : bool, build_command : list):
+
+    #some packages require building in a directory other than the source's root
+    if(separate_build_dir):
+        build_dir = Path(source_root / "build")
+    else:
+        build_dir = source_root
+
+    #make sure the build directory exist beore making it the working directory
+    if(build_dir.exists()):
+        os.chdir(build_dir)
+    else:
+        print("build directory doesn't exist")
+        return 1
+
+    #configure the source
+    try:
+        subprocess.run(config_command)
+    except subprocess.CalledProcessError as e:
+        print("error: " + str(config_command[0]) +
+              " exited " + str(e.returncode))
+        return 2
+
+    print("source configuration succesfull")
+
+    #build source (unless config_only is true)
+    if(not config_only):
+        try:
+            subprocces.run(build_command)
+        except subprocess.CalledProcessError as e:
+            print("error: " + str(build_command[0]) +
+                  "exited " + str(e.returncode))
+            return 3
+        print("build successfull")
+    return 0
